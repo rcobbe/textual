7584d391428a52b156da8699aa7864618dc3dc09
Richard Cobbe <rcobbe@rcobbe.net>
2017-04-22 13:08:16 -0400

    Bump to version 2.0; add Apache v2 license

--- Data/Textual.hs
+++ Data/Textual.hs
@@ -1,5 +1,21 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 
+{-
+Copyright 2013-2017 Richard Cobbe
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-}
+
 -- | Define a general interface to a string type, to allow us to defer the
 --   decision about the precise string representation as late as possible.
 --   There is an unfortunate interaction with overloaded strings in programs
